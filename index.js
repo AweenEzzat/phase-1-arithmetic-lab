fb8c9e88ab7b5fba973326c336ff702f50d530e2
Aween Ezzat <aweenezzat@gmail.com>
2024-02-21 15:51:54 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+
+// function random() {
+    
+//     return Math.ceil(Math.random() * 100);
+// }
+let num1=2;
+let num2=31;
+let multiply = num1*num2;
+
+let random = Math.ceil(Math.random() * 100);
+
+let num3 = 32;
+let num4 = 7;
+let mod = num3%num4;
+
+let numbers = [3, 7, 12, 20, 15];
+let max = Math.max(...numbers);
